e0ace87df4756f3b48f9388b3e41d569067090d1
Mateusz Kotwiński <mkotwinski98@gmail.com>
2020-08-05 12:37:36 +0200

    Updates

---
+++ precache-manifest.87a7d86ec1d185c8958dff1c606f92f3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98d38fe5b70a707e3381fdeae3cc9b2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "962fa52515edb0241a15",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1089c631179df929c9c1",
+    "url": "/portfolio/static/css/main.d5c8b940.chunk.css"
+  },
+  {
+    "revision": "962fa52515edb0241a15",
+    "url": "/portfolio/static/js/2.72c01541.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.72c01541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1089c631179df929c9c1",
+    "url": "/portfolio/static/js/main.fc26fd7e.chunk.js"
+  },
+  {
+    "revision": "8641036e6995cd6d40a9",
+    "url": "/portfolio/static/js/runtime-main.f50d65dc.js"
+  },
+  {
+    "revision": "76f8bde949ed3e22aa218aae75b62719",
+    "url": "/portfolio/static/media/Blog-App.76f8bde9.png"
+  },
+  {
+    "revision": "68a4a02bd9452b9317435364f1109e53",
+    "url": "/portfolio/static/media/Calculator.68a4a02b.png"
+  },
+  {
+    "revision": "7f8336020a3d5363d1e8d8682c05526f",
+    "url": "/portfolio/static/media/FurnituresShop.7f833602.png"
+  },
+  {
+    "revision": "54f39718a6bc2e5d5df376652fe144cd",
+    "url": "/portfolio/static/media/Portfolio.54f39718.png"
+  },
+  {
+    "revision": "0b1378e765c146bb690157fc81f3e31b",
+    "url": "/portfolio/static/media/WeatherApp.0b1378e7.png"
+  },
+  {
+    "revision": "8dd70cd2064d2f30d4302ccf731a6e54",
+    "url": "/portfolio/static/media/toDoApp.8dd70cd2.png"
+  }
+]);
