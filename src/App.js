e7c8149668ec51f1092c3915e01a2fa933451d25
Mateusz Kotwinski <mateuszkotwinski@PLMN-380617.local>
2024-03-28 07:24:20 +0100

    Remove some code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Menu from './components/layouts/Menu';
-import Main from './components/layouts/Main';
-import Footer from './components/layouts/Footer';
+import Menu from "./components/layouts/Menu";
+import Main from "./components/layouts/Main";
+import Footer from "./components/layouts/Footer";
 
 import { Container } from "react-bootstrap";
 
 class App extends Component {
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Container>
           <Menu />
           <Main />
